835494e1f91aa03748a850dcc1752968352d6400
Jen Caza <jencaza@gmail.com>
2021-11-22 20:21:02 -0500

    code to return tail of array

---
+++ lotide/tail.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢✅🟢 Assertion Passed:  ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed:  ${actual} !== ${expected}`);
+  }
+
+};
+
+const tail = function(array) {
+  let newArray = array.slice(1);
+  return newArray;
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+const words1 = ["Yo Yo"];
+tail(words1);
+assertEqual(words1.length, 1);
+
+const words0 = [];
+tail(words0);
+assertEqual(words0.length, 0);
