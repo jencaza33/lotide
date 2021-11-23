185d3bb5778d1621603a4b801b900363d56cc25c
Jen Caza <jencaza@gmail.com>
2021-11-22 20:01:13 -0500

    created code to return 1st item

---
+++ lotide/head.js
@@ -0,0 +1,27 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢✅🟢 Assertion Passed:  ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed:  ${actual} !== ${expected}`);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+
+const head = function(array) {
+  return array[0];
+};
+
+
+// TEST CODE
+
+assertEqual(head([5,6,7]), 5); // pass
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // pass
+assertEqual(head(["one"]), "one"); // pass
+assertEqual(head([]), ); // undefined
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs"); // failYeah
