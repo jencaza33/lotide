9e60b31ebdf0fc57c5d5eefcf174efb5a8a4d14c
Jen Caza <jencaza@gmail.com>
2021-11-27 22:36:05 -0500

    improved code

--- lotide/countOnly.js
+++ lotide/countOnly.js
@@ -1,11 +1,13 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`🟢✅🟢 Assertion Passed:  ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed:  ${actual} !== ${expected}`);
+    console.log(`😃😃😃😃 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`😡😡😡😡Assertion Failed: ${actual} !== ${expected}`);
   }
-
 };
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
 
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
