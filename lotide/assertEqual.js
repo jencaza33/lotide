e3e700e7810faff5e31e018584e471ea830ace35
Jen Caza <jencaza@gmail.com>
2021-11-27 22:52:12 -0500

    improved code

--- lotide/assertEqual.js
+++ lotide/assertEqual.js
@@ -1,13 +1,10 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`🟢✅🟢 Assertion Passed:  ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed:  ${actual} !== ${expected}`);
+    console.log(`😃😃😃😃 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`😡😡😡😡Assertion Failed: ${actual} !== ${expected}`);
   }
-
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
