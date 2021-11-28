cd9edf9fb426badcfaec8d3e3bf3a624b342fc28
Jen Caza <jencaza@gmail.com>
2021-11-27 22:54:33 -0500

    improved code

--- lotide/tail.js
+++ lotide/tail.js
@@ -1,10 +1,9 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`🟢✅🟢 Assertion Passed:  ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed:  ${actual} !== ${expected}`);
+    console.log(`😃😃😃😃 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`😡😡😡😡Assertion Failed: ${actual} !== ${expected}`);
   }
-
 };
 
 const tail = function(array) {
