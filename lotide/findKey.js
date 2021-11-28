d5531bb9b64689ecd790427fcdd4751ea0ee8717
Jen Caza <jencaza@gmail.com>
2021-11-27 23:35:36 -0500

    fixed linting errors

--- lotide/findKey.js
+++ lotide/findKey.js
@@ -44,7 +44,6 @@ assertEqual(findKey({ "Blue Hill": { stars: 1 },
   "Akelarre":  { stars: 3 }
 }, x => x.stars === 4), undefined);
 
-module.exports = findKey;
 
 // const callback = function(x){
 // if (x.stars ===2){
