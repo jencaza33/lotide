10b52c5402fa45737c2e8750fcc7f951eba518ac
Jen Caza <jencaza@gmail.com>
2021-11-29 20:23:52 -0500

    index of exported modules in shorthand

---
+++ index.js
@@ -0,0 +1,16 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const assertArrayEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  assertArrayEqual,
+  assertEqual,
+  eqArrays,
+};
